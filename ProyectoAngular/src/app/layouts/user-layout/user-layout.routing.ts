2580379ceab8b3d3b4aa9b24ad2c1cb3024a1b90
Alfreed0 <lurochav@espol.edu.ec>
2022-06-13 10:06:15 -0500

    Pagina de login y registro

--- ProyectoAngular/src/app/layouts/user-layout/user-layout.routing.ts
+++ ProyectoAngular/src/app/layouts/user-layout/user-layout.routing.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 
 import { PackagesComponent } from 'src/app/pages/packages/packages.component';
+import { LoginRegisterComponent } from 'src/app/pages/login-register/login-register.component';
 
 export const UserLayoutRoutes: Routes = [
     { path: 'paquetes',          component: PackagesComponent},
+    { path: 'login',             component: LoginRegisterComponent }
 ];
