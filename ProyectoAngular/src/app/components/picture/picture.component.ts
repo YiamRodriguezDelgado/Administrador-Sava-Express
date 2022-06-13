28bffc8094e22aa575b7b2d032db5b8ec543a1c8
Carlos Meneses <cmeneses@espol.edu.ec>
2022-06-13 00:09:33 -0500

    CreacionPaquetes

---
+++ ProyectoAngular/src/app/components/picture/picture.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-picture',
+  templateUrl: './picture.component.html',
+  styleUrls: ['./picture.component.scss']
+})
+export class PictureComponent {
+  @Input() url:string;
+  @ViewChild("picture") pictureShow:any;
+  constructor(private modalService: NgbModal) { }
+
+  CreateModal(name:any){
+    this.url=name;
+    this.modalService.open(this.pictureShow, {
+      centered: true,
+      backdrop: 'static',
+      size: 'm'
+    });
+  }
+  onClose() {
+    this.modalService.dismissAll();
+  }
+
+}
