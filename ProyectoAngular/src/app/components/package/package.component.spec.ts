ce37a9835946c2c071348300935ff91f5f129017
Carlos Humberto Meneses Murillo <66740049+Cmenesess@users.noreply.github.com>
2022-07-02 21:42:31 -0500

    Update package.component.spec.ts

--- ProyectoAngular/src/app/components/package/package.component.spec.ts
+++ ProyectoAngular/src/app/components/package/package.component.spec.ts
@@ -1,11 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { NgbModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { PackageComponent } from './package.component';
+import { By } from '@angular/platform-browser';
+import { Package } from '../../models/package';
+export class MockNgbModalRef {
+  componentInstance = {
+      prompt: undefined,
+      title: undefined
+  };
+  result: Promise<any> = new Promise((resolve, reject) => resolve(true));
+}
 
 describe('PackageComponent', () => {
   let component: PackageComponent;
   let fixture: ComponentFixture<PackageComponent>;
-
+  let modalService: NgbModal;
+  let mockModalRef: MockNgbModalRef = new MockNgbModalRef();
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ PackageComponent ]
@@ -16,10 +26,22 @@ describe('PackageComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PackageComponent);
     component = fixture.componentInstance;
+    const paquete_Prueba :Package ={trackingNumber:124,foto:'../../../assets/img/icons/common/package.jpg',precio:90,peso:"../../../assets/img/icons/common/package.jpg",fechaLLegada:'Mañana'};
+    component.package=paquete_Prueba;
     fixture.detectChanges();
+    modalService = TestBed.get(NgbModal);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  
+  it('should open modal', () => {
+    spyOn(modalService, 'open').and.returnValue(mockModalRef as any);
+    const productSelectElement = fixture.debugElement.query(By.css('.image-details'));
+    productSelectElement.triggerEventHandler("click",123);
+    fixture.detectChanges();
+    expect(modalService.open).toHaveBeenCalled();
+  });
+
 });
