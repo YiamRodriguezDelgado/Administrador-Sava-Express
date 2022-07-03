246bd606937ebde9e16a5f3fd10f287a231eb716
Carlos Humberto Meneses Murillo <66740049+Cmenesess@users.noreply.github.com>
2022-07-02 21:41:11 -0500

    Update sent.component.spec.ts

--- ProyectoAngular/src/app/pages/sent/sent.component.spec.ts
+++ ProyectoAngular/src/app/pages/sent/sent.component.spec.ts
@@ -1,6 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { DebugElement } from "@angular/core";
 import { SentComponent } from './sent.component';
+import {FormControl} from '@angular/forms';
+import { Package } from 'src/app/models/package';
+import { ClientsDataService } from 'src/app/service/clients-data.service';
+import Swal from 'sweetalert2';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import * as exp from 'constants';
+import { By } from '@angular/platform-browser';
 
 describe('SentComponent', () => {
   let component: SentComponent;
@@ -8,7 +15,9 @@ describe('SentComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SentComponent ]
+      declarations: [ SentComponent],
+      imports: [HttpClientTestingModule],
+      providers: [ClientsDataService]
     })
     .compileComponents();
   });
@@ -19,7 +28,24 @@ describe('SentComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should send error when the list is empty', () => {
+    component.enviar();
+    expect(Swal.isVisible()).toBeTruthy();
+    expect(Swal.getTitle().textContent).toBe("Seleccione al menos un paquete");
   });
+
+  it("should send a right if there it's a list one package selected",()=>{
+    component.selectedPackages.setValue('1234')
+    component.enviar();
+    expect(Swal.getTitle().textContent).toBe("Acabas de crear tu paquete Sava");
+  })
+
+  it("should been adding package to the package container",()=>{
+    const numeroPrueba=123;
+    fixture.detectChanges();
+    const productSelectElement = fixture.debugElement.query(By.css('mat-select'));
+    productSelectElement.triggerEventHandler('selectionChange',{ value:  [numeroPrueba] });
+    expect(component.paquetesMostrar.length).toBe(1);
+  })
 });
+
